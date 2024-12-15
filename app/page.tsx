c8b448660e6adf36726eb6b4bbb75c20004ee8be
soorokim <rlatnfh99@gmail.com>
2024-12-15 23:53:01 +0900

    supabase setting

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      soolog에 오신걸 환영합니다. 업데이트가 되나요?.
-    </div>
-  );
+import { createClient } from '@/utils/supabase/server';
+
+export default async function Home() {
+  const supabase = await createClient();
+  const { data: posts } = await supabase.from('posts').select();
+  return <div>{JSON.stringify(posts, null, 2)}</div>;
 }
